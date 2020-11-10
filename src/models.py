9e52564862ae763e38b16de76f2e0142582c4737
alexcastillla <alexander.zcas@gmail.com>
2020-11-10 12:12:29 +0000

    Added DB on Models.py

--- src/models.py
+++ src/models.py
@@ -4,16 +4,29 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    children = db.relationship('Todo', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "username": self.username,
             # do not serialize the password, its a security breach
+        }
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f'label: {self.label}, done: {self.done}'
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "done": self.done,
         }
